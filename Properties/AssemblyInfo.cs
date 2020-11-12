73be494145f29c9e0200122ff0f80a0a58954961
harry stanyer <harry.stanyer@sigma.se>
2020-11-12 10:20:48 +0000

    jeez

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UmbracoTestSite")]
+[assembly: AssemblyTitle("UmbracoTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UmbracoTestSite")]
+[assembly: AssemblyProduct("UmbracoTest")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a7ba3d5d-e60b-4eb0-921b-adec66079a6b")]
+[assembly: Guid("716a4d7b-9402-4b16-84c1-364b46847b11")]
 
 // Version information for an assembly consists of the following four values:
 //
